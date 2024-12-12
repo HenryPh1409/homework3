45cbf7f46e00fa61956449b9d683ff17459befb9
HenryPh1409 <147539521+HenryPh1409@users.noreply.github.com>
2024-12-12 20:02:31 +0800

    Update question_4.cpp

--- hw_1123561/question_4.cpp
+++ hw_1123561/question_4.cpp
@@ -1,61 +1,89 @@
 //Henry-1123561
-//28-11-2024
+//11-12-2024
+
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
+
 using namespace std;
 
-struct Task {
-    int profit;
-    int deadline;
-
-    bool operator<(const Task& other) const {
-        return profit < other.profit;
-    }
-};
-
-int main() {
-    int N;
-    cin >> N;
-    vector<Task> tasks(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> tasks[i].profit >> tasks[i].deadline;
-    }
-
-    // Sort tasks by profit in descending order
-    sort(tasks.rbegin(), tasks.rend());
-
-    int maxDeadline = 0;
-    for (const Task& task : tasks) {
-        maxDeadline = max(maxDeadline, task.deadline);
-    }
-
-    // Use a priority queue to schedule the tasks
-    priority_queue<int, vector<int>, greater<int>> pq;
-    int totalProfit = 0;
-    vector<int> scheduledTasks;
-
-    for (const Task& task : tasks) {
-        if (pq.size() < task.deadline) {
-            pq.push(task.profit);
-            totalProfit += task.profit;
-            scheduledTasks.push_back(task.profit);
-        } else if (!pq.empty() && pq.top() < task.profit) {
-            totalProfit += task.profit - pq.top();
-            pq.pop();
-            pq.push(task.profit);
-            scheduledTasks.push_back(task.profit);
+// Disjoint Set Union class to manage connected components
+class DisjointSetUnion {
+public:
+    DisjointSetUnion(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
         }
     }
 
-    cout << "Maximum Profit: " << totalProfit << endl;
-    cout << "Scheduled Tasks: [";
-    for (int i = 0; i < scheduledTasks.size(); ++i) {
-        cout << scheduledTasks[i];
-        if (i < scheduledTasks.size() - 1) cout << ", ";
+    int find(int u) {
+        if (parent[u] != u) {
+            parent[u] = find(parent[u]);
+        }
+        return parent[u];
     }
-    cout << "]" << endl;
+
+    void unionSets(int u, int v) {
+        int rootU = find(u);
+        int rootV = find(v);
+        if (rootU != rootV) {
+            if (rank[rootU] > rank[rootV]) {
+                parent[rootV] = rootU;
+            } else if (rank[rootU] < rank[rootV]) {
+                parent[rootU] = rootV;
+            } else {
+                parent[rootV] = rootU;
+                rank[rootU]++;
+            }
+        }
+    }
+
+private:
+    vector<int> parent;
+    vector<int> rank;
+};
+
+int kruskal(int V, vector<vector<int>>& edges) {
+    DisjointSetUnion dsu(V);
+    sort(edges.begin(), edges.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a[2] < b[2];
+    });
+
+    int mst_weight = 0;
+    int mst_edges = 0;
+
+    for (const auto& edge : edges) {
+        int u = edge[0];
+        int v = edge[1];
+        int weight = edge[2];
+        if (dsu.find(u) != dsu.find(v)) {
+            dsu.unionSets(u, v);
+            mst_weight += weight;
+            mst_edges++;
+            if (mst_edges == V - 1) {
+                break;
+            }
+        }
+    }
+
+    return mst_weight;
+}
+
+int main() {
+    int V = 4;
+    vector<vector<int>> edges = {
+        {0, 1, 3},
+        {0, 2, 6},
+        {0, 3, 4},
+        {1, 3, 5},
+        {2, 3, 4}
+    };
+
+    int mst_weight = kruskal(V, edges);
+    cout << mst_weight << endl;
 
     return 0;
 }
+
